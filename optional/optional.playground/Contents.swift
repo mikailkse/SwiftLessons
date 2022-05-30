3dbab817dd0456ca977cd75101d983cc287ff6ad
Mikail Köse <41960652+mikailkse@users.noreply.github.com>
2022-05-30 06:16:43 +0300

    optional - 30/05/22

---
+++ optional/optional.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var str:String?
+
+str = "Hello"
+
+if let temp = str {
+    print(temp)
+} else {
+    print(":(")
+}
+
+
+var number = "56"
+
+
+if let abc = Int(number) {
+    print(abc)
+} else {
+    print(":(")
+}
